a4ed1c30639ca56f3fe304f7f0f50516f8dc7cda
Peter Osoo <posoo617@gmail.com>
2024-03-13 17:50:01 +0300

    change JSX

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,12 @@
 const Todo = ({ todo, removeTodo }) => {
 	return (
-		<li
-			key={todo.id}
-			className="list-group-item d-flex justify-content-between align-items-center"
-		>
+		<li className="list-group-item d-flex justify-content-between align-items-center">
 			{todo.text}
 			<button
 				onClick={() => removeTodo(todo.id)}
 				className="btn btn-danger btn-sm"
 			>
-				Remove
+				Remove {todo.id}
 			</button>
 		</li>
 	)
